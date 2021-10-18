369c081a10f3e914180c19c1cd794ba534d39979
Josafa01 <josafa_correa_fonseca@hotmail.com>
2021-10-18 09:53:33 -0200

    Listando todos os livros por categoria

--- src/main/java/com/bookstore/bookstore/service/LivroService.java
+++ src/main/java/com/bookstore/bookstore/service/LivroService.java
@@ -1,5 +1,7 @@
 package com.bookstore.bookstore.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,17 @@ public class LivroService {
 	
   @Autowired
   private LivroRepository repository;
+
+  @Autowired
+  private CategoriaService categoriaService;
   
   public Livro findById(Integer id) {
 	  java.util.Optional<Livro> obj = repository.findById(id);
 	  return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " +id + ", Tipo: " + Livro.class.getName()));
   }
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return repository.findAllCategoria(id_cat);
+	}
 }
