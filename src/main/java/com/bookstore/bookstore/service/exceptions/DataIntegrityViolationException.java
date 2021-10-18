cd85e92c7839a5e99dc816153f472c9c2487a29e
Josafa01 <josafa_correa_fonseca@hotmail.com>
2021-10-18 03:49:39 -0200

    Exececao personalizada para deletar categoria

---
+++ src/main/java/com/bookstore/bookstore/service/exceptions/DataIntegrityViolationException.java
@@ -0,0 +1,19 @@
+package com.bookstore.bookstore.service.exceptions;
+
+public class DataIntegrityViolationException  extends RuntimeException{
+	
+	private static final long serialVersionUID = 1l;
+	
+	public DataIntegrityViolationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataIntegrityViolationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
