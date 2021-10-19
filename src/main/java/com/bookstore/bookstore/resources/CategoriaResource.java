0ef91461883138033f8390e93fd681a70b6819d0
Josafa01 <josafa_correa_fonseca@hotmail.com>
2021-10-19 01:35:28 -0300

    cross origin

--- src/main/java/com/bookstore/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/bookstore/bookstore/resources/CategoriaResource.java
@@ -24,6 +24,7 @@ import com.bookstore.bookstore.dtos.CategoriaDTO;
 import com.bookstore.bookstore.service.CategoriaService;
 
 @CrossOrigin("*")
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
